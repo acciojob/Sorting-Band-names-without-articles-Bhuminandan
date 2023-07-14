877e9806ae3294cbdf3b9ceeff246024889d3e97
Bhuminandan <134225833+Bhuminandan@users.noreply.github.com>
2023-07-14 18:09:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
-function strip (word) {
-    let regex = new RegExp('^(a |the |an )', 'i')
-    return word.replace(regex, '').trim()
+function strip(bandname) {  
+  return bandname.replace(/^(a |the |an)/i, '').trim()
 }
 
-const sortedBands = bands.sort((a, b) => (strip(a) > strip(b)) ? 1 : -1)
+const sortedBands = touristSpots.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
 
-
-document.querySelector('#bands').innerHTML = 
-    sortedBands.map(item => `<li>${item}</li>`).join('')
-
-console.log(sortedBands);
+document.getElementById( '#bands' ).innerHTML = sortedBands.map( ( b ) => `<li>${b}</li>` ).join('');
